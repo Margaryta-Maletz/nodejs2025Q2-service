e060b56b632b7a3170a17a50f9cce446564d82cd
Marharyta Malets <marharyta_malets@epam.com>
2025-05-30 00:47:48 +0300

    feat: add user requests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './create-user.dto';
+import { UpdatePasswordDto } from './update-password.dto';
+import { StatusCodes } from 'http-status-codes';
 
-@Controller()
+@Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
-  constructor(private readonly appService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.getUser(id);
+  }
+
+  @Post()
+  post(
+    @Body(new ValidationPipe({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }))
+    dto: CreateUserDto,
+  ) {
+    return this.userService.post(dto);
+  }
+
+  @Put(':id')
+  put(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body(new ValidationPipe({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }))
+    dto: UpdatePasswordDto,
+  ) {
+    return this.userService.put(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(StatusCodes.NO_CONTENT)
+  delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.delete(id);
   }
 }
