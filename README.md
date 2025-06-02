8f9169e26ceeadfbf764403a213d5c5dc37e4db5
Marharyta Malets <marharyta_malets@epam.com>
2025-06-02 19:48:25 +0300

    feat: add swagger

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ npm install
 npm start
 ```
 
-After starting the app on port (4000 as default) you can open
+After starting the app on port (4000 default or **change to the PORT in use**) you can open
 in your browser OpenAPI documentation by typing http://localhost:4000/doc/.
 For more information about OpenAPI/Swagger please visit https://swagger.io/.
 
@@ -53,18 +53,6 @@ npm run test -- artists.e2e.spec.ts
 npm run test -- users.e2e.spec.ts
 npm run test -- albums.e2e.spec.ts
 npm run test -- tracks.e2e.spec.ts
-```
-
-To run all test with authorization
-
-```
-npm run test:auth
-```
-
-To run only specific test suite with authorization
-
-```
-npm run test:auth -- <path to suite>
 ```
 
 ### Auto-fix and format
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { parse } from 'yaml';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
 
 dotenv.config();
 
@@ -17,7 +21,13 @@ async function bootstrap() {
     }),
   );
 
+  const path = join(__dirname, '..', 'doc', 'api.yaml');
+  const content = await readFile(path, { encoding: 'utf8' });
+  const document = parse(content);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger documentation: http://localhost:${port}/doc`);
 }
 bootstrap();
